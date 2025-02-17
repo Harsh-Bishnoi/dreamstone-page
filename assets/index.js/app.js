4f4c85d716d48c076a05c85bdb8e8705d699381b
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-17 15:58:09 +0530

    half footer responsive

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -17,10 +17,10 @@ $('.my-slider').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 infinite: true,
-                dots: false
+                dots: true
             }
         },
         {
@@ -28,7 +28,7 @@ $('.my-slider').slick({
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                dots: false
+                dots: true
             }
         },
         {
@@ -36,7 +36,7 @@ $('.my-slider').slick({
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                dots: false
+                dots: true
             }
         }
     ]
