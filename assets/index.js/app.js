e673f1df21fab1e7bb9fe55c6abcdc9d291ec331
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-17 15:02:02 +0530

    header done

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -5,3 +5,39 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.my-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        }
+    ]
+});
